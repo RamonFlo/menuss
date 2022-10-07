9c92bdd6397b21f005770c9e74bab32df4782e8d
Alumno <Alumno@PC-39>
2022-10-07 12:24:31 -0500

    Agregar archivos de proyecto.

---
+++ menuss/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace menuss
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void abirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Stream stm;
+            OpenFileDialog ofd = new OpenFileDialog(); 
+            if(ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                if ((stm = ofd.OpenFile()) != null)
+                {
+                    string str = ofd.FileName;
+                    string ftxt = File.ReadAllText(str);
+                    richTextBox1.Text = ftxt;
+                }
+
+            }
+            
+        }
+
+        private void guardarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Archivo de texto(*.txt)|*.txt|Archivo de C#(*.cs)|*.cs";
+            sfd.ShowDialog();
+            File.WriteAllText(sfd.FileName, richTextBox1.Text);
+            MessageBox.Show("Archivo de texto guardado exitosamente.")
+        }
+
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
